1cf75ab2b1e1ddad5981d22d9cd03aa933703ffd
proll <gleb.proll@gmail.com>
2014-01-09 23:09:48 +0400

    404 fix

--- app/scripts/l10n/l10n__404-page.js
+++ app/scripts/l10n/l10n__404-page.js
@@ -9,14 +9,6 @@ qst.l10n.p404 = {
         zh: '请输入正确的URL'
 	},
 
-	'You can also Search for this user, photo or story;': {
-		en: 'You can also Search for this user, photo or story;',
-		ru: 'Вы также можете попробовать Найти этого пользователя, фотографию или историю;',
-		ko: '이 사용자, 포토 또는 스토리를 검색할 수 있어요.',
-        ja: 'このユーザー、フォト、ストーリーを検索することも可能です。',
-        zh: '你也可以搜索这个用户、照片或者故事。'
-	},
-
 	'or drop us a line to <a href="mailto:support@qstoq.me" class="lnk">support@qstoq.me.</a>': {
 		en: 'or drop us a line to <a href="mailto:support@qstoq.me" class="lnk">support@qstoq.me.</a>',
 		ru: 'Или напишите нам — <a href="mailto:support@qstoq.me" class="lnk">support@qstoq.me</a>',
