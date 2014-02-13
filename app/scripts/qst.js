a07afd712e9e00fe68c3da5d3c790c57797f2046
proll <gleb.proll@gmail.com>
2014-02-13 14:17:51 +0400

    lanf no auto

--- app/scripts/qst.js
+++ app/scripts/qst.js
@@ -1,7 +1,8 @@
 if (typeof console == "undefined") {this.console = {log: function() {}, error: function() {}};}
 window.qst = window.qst || {};
 window.qst = _.extend(window.qst, {
-	language: _.getCookie('lang') || (navigator.language || navigator.systemLanguage || navigator.browserLanguage || navigator.userLanguage || 'en').substr(0, 2).toLowerCase(),
+	// language: _.getCookie('lang') || (navigator.language || navigator.systemLanguage || navigator.browserLanguage || navigator.userLanguage || 'en').substr(0, 2).toLowerCase(),
+	language: _.getCookie('lang') || 'ru',
 	root: "http://qstoq.me",
 	l10n: {},
 
